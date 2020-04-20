398b6207efc9beda9c6de1b7ff49db9aae9b2cbb
Lion Kapitman <l.kapitman@gmail.com>
2020-04-20 23:33:11 +0200

    wite 3d model

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ ax = axes3d.Axes3D(plt.figure())
 
 i = np.arange(-10, 10, 0.1)
 X, Y = np.meshgrid(i, i)
-Z = X * np.sin(X + Y) + Y * 2
+pi = np.pi **2
+Z = X * np.sin(X + Y) + Y + pi * 2
 
 ax.plot_wireframe(X, Y, Z, rstride=10, cstride=10)
 
